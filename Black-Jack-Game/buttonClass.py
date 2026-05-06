dc99d50e83fbd137b168d741d78f191857c619d2
isieann <bellastaffier@gmail.com>
2026-05-06 12:18:13 -0400

    Folder

---
+++ Black-Jack-Game/buttonClass.py
@@ -0,0 +1,9 @@
+class Button:
+    def __init__(self,x,y,f,pf):
+        self.x = x
+        self.y = y
+        self.file = f
+        self.downFile = pf
+        
+    def __str__():
+        pass
--- buttonClass.py
+++
@@ -1,9 +0,0 @@
-class Button:
-    def __init__(self,x,y,f,pf):
-        self.x = x
-        self.y = y
-        self.file = f
-        self.downFile = pf
-        
-    def __str__():
-        pass
